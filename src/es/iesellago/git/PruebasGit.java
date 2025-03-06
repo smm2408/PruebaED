edfd933d94c7cb79ef5c94cff2700ba48ef91bd4
Santiago Medina Martínez <smm2022sdc@gmail.com>
2025-03-06 20:40:46 +0100

    Segundo commit realizado

--- src/es/iesellago/git/PruebasGit.java
+++ src/es/iesellago/git/PruebasGit.java
@@ -5,6 +5,7 @@ public class PruebasGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer commit realizado");
+		System.out.println("Segundo commit realizado");
 	}
 
 }
