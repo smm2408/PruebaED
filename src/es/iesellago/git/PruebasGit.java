c863f2ead7a992e02f056428d5a07378f59acc17
Santiago Medina Martínez <smm2022sdc@gmail.com>
2025-03-06 20:38:53 +0100

    Subida inicial del proyecto

---
+++ src/es/iesellago/git/PruebasGit.java
@@ -0,0 +1,10 @@
+package es.iesellago.git;
+
+public class PruebasGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primer commit realizado");
+	}
+
+}
